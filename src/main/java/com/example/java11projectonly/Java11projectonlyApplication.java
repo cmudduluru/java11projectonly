74145c09b480388e94b7c00030c71606adde6dd1
cmudduluru <chandrika.mudduluru@actian.com>
2021-08-06 13:15:07 -0500

    Initial commit

---
+++ src/main/java/com/example/java11projectonly/Java11projectonlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.java11projectonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11projectonlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java11projectonlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java11projectonly/Java11projectonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java11projectonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11projectonlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
